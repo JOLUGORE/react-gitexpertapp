0b5b8ffb7cce4cee208d32a48fdc7d1818d3ae59
JoseLuis <jl_greyes@hotmail.com>
2020-07-20 10:09:59 -0500

    docs agregado

---
+++ docs/precache-manifest.890c20b899587ab355afc7ea80771d64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9587dc78f3c0c5864961d4fc998574f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c7d885cdf611ce84c68",
+    "url": "/static/css/main.4b0e404a.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7d885cdf611ce84c68",
+    "url": "/static/js/main.1db0b557.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
